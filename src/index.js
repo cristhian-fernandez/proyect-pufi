1e35049d367687d66e3a14b93a12e738ac7acd52
Cristhian Fernandez <cjfernandez29@gmail.com>
2022-09-30 07:07:49 -0500

    lastest changes proyect pufi

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../src/redux/store/index';
-// import dotenv from 'dotenv';
-// import axios from 'axios';
-
-// dotenv.config();
-// axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
